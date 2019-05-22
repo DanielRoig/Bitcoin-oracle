f77e6bfaf05e37e1f0a505afc1ffd3bc88847182
XavoPls <xavi.oliza@gmail.com>
2019-05-22 17:48:13 +0200

    - Upgraded to python 3.7.3 - Added requirements.txt - Updated .gitignore to work with venv

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import twitterCrawler as crawler
 import bitcoinData as btc
 
-twitterCrawler=crawler.Crawler("Bitcoin",1,"en")
+twitterCrawler = crawler.Crawler("Bitcoin", 1, "en")
 twitterCrawler.track()
 
 for tweet in twitterCrawler.myTweets:
-	print tweet.tweet_text
-	print tweet.tweet_date
-	print tweet.user_nickname
-	print tweet.user_id
-	print tweet.user_followers
+    print(tweet.tweet_text)
+    print(tweet.tweet_date)
+    print(tweet.user_nickname)
+    print(tweet.user_id)
+    print(tweet.user_followers)
 
-	print "----------------------------"
+    print("----------------------------")
 
-
-
-bitcoin=btc.BitcoinData()
-print bitcoin.averageData()
+bitcoin = btc.BitcoinData()
+print(bitcoin.averageData())
--- twitterCrawler.py
+++ twitterCrawler.py
@@ -1,42 +1,48 @@
 import tweepy
 import datetime
 
-class Crawler():
-	def __init__(self, keyword, hours, lang):
-		self.keyword = keyword
-		self.hours = hours
-		self.lang = lang
-		self.myTweets = []
 
-		self.auth = tweepy.OAuthHandler("PxrgqjQnWgGvfATB4hWXlyshv", "SNKu5GeyPCFIpRpWI7cFIEsKud4N7cstZqTT1qJNcO5PzovkiG")
-		self.auth.set_access_token("935832971390738433-PeGkE4PPapZSYjPOdXqeezH5O2lVjvf", "VwW75cuyO38sCWaJ7nfHbtmF3YHlBoJ4PSzGx903w2qxl")
-		self.api = tweepy.API(self.auth)
+class Crawler:
+    def __init__(self, keyword, hours, lang):
+        self.keyword = keyword
+        self.hours = hours
+        self.lang = lang
+        self.myTweets = []
 
-	def track(self):
-		time_first_tweet=0
-		halt=True;
-		last_id = -1
+        self.auth = tweepy.OAuthHandler("PxrgqjQnWgGvfATB4hWXlyshv",
+                                        "SNKu5GeyPCFIpRpWI7cFIEsKud4N7cstZqTT1qJNcO5PzovkiG")
+        self.auth.set_access_token("935832971390738433-PeGkE4PPapZSYjPOdXqeezH5O2lVjvf",
+                                   "VwW75cuyO38sCWaJ7nfHbtmF3YHlBoJ4PSzGx903w2qxl")
+        self.api = tweepy.API(self.auth)
 
-		while halt:
-			try:
-				new_tweets = self.api.search(q=self.keyword, count=200, lang='en', max_id=str(last_id - 1))
-				if not new_tweets:
-					halt=False
+    def track(self):
+        time_first_tweet = 0
+        halt = True;
+        last_id = -1
 
-				for tweet in new_tweets:
-        	
-					if time_first_tweet==0:
-						time_first_tweet=tweet.created_at
+        while halt:
+            try:
+                new_tweets = self.api.search(q=self.keyword, count=200, lang='en', max_id=str(last_id - 1))
+                if not new_tweets:
+                    halt = False
 
-					elif (time_first_tweet-tweet.created_at).total_seconds() > self.hours*3600:
-						halt=False
+                for tweet in new_tweets:
 
-					else:
-						self.myTweets.append(Tweet(tweet.text, tweet.created_at, tweet.author.screen_name, tweet.author.id, tweet.author.followers_count))
-					last_id = new_tweets[-1].id
+                    if time_first_tweet == 0:
+                        time_first_tweet = tweet.created_at
 
-			except tweepy.TweepError as e:
-				halt=False
+                    elif (time_first_tweet - tweet.created_at).total_seconds() > self.hours * 3600:
+                        halt = False
+
+                    else:
+                        self.myTweets.append(
+                            Tweet(tweet.text, tweet.created_at, tweet.author.screen_name, tweet.author.id,
+                                  tweet.author.followers_count))
+                    last_id = new_tweets[-1].id
+
+            except tweepy.TweepError as e:
+                halt = False
+
 
 class Tweet:
     def __init__(self, tweet_text, tweet_date, user_nickname, user_id, user_followers):
@@ -46,6 +52,3 @@ class Tweet:
         self.user_id = user_id
         self.user_followers = user_followers
         self.behaviour = 0
-
-
-
