34bde58c502af05eb361aebe4d43765d8588637a
Xavopls <xavi.oliza@gmail.com>
2019-06-01 18:30:31 +0200

    Added Tweet class

---
+++ Tweet.py
@@ -0,0 +1,10 @@
+
+
+class Tweet:
+    def __init__(self, tweet_text, tweet_date, user_nickname, user_id, user_followers):
+        self.tweet_text = tweet_text
+        self.tweet_date = tweet_date
+        self.user_nickname = user_nickname
+        self.user_id = user_id
+        self.user_followers = user_followers
+        self.behaviour = 0
