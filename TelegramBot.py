7a27cf865947061e10fac4c27867b88bcc25a23d
Daniel <daniielroig@gmail.com>
2019-06-04 19:31:14 +0200

    botty

--- TelegramBot.py
+++ TelegramBot.py
@@ -4,30 +4,71 @@ from Utils import retrieve_tweets
 TOKEN = '867716809:AAGcvZb7K4-zeOWFpNv-nh9bqo0D0TiDCew' 
 
 bot = telebot.TeleBot(TOKEN)
+client_list=[]
 
-def mainMarkup():
-    markup = types.ReplyKeyboardMarkup(row_width=1)
-    itembtn1 = types.KeyboardButton('Show the analysis of the last hour')
-    itembtn2 = types.KeyboardButton('Give me the prize of Bitcoin')
-    markup.add(itembtn1, itembtn2)
-    return markup
 
+def setClient(client, coin='', hour=0):
+    index =checkClient(client)
+
+    if coin !='':
+        client_list[index][1]=coin
+    if hour!=0:
+        client_list[index][2]=hour
+
+def resetClient(client):
+    index =checkClient(client)
+
+    client_list[index][1]=''
+    client_list[index][2]=0
+
+def coinExist(coin):
+    return True
+
+def checkClient(client):
+    for index, client in enumerate(client_list):
+        if client[0]==client:
+            return index #Existe
+    return False   #No existe
+
+
+
+
+    
+         
 
 @bot.message_handler(commands=['start', 'help'])
 def command_help(message):
-        bot.send_sticker(message.chat.id, "CAADAQADAQADHYJ7HYCiE4crvJ4kAg")
-        bot.send_message(message.chat.id, "✅✅ He he hey! My name is Bitcoin Oracle and I'm so excited to help you and to be your favourite trending tool")
-        bot.send_message(message.chat.id, "With this bot you can analyze all tweets in the last hours of Bitcoin. It enable to you make decisions that can take huge profits. If you want to be financially independently, Bitcoin Oracle is you best mate. 🚀🚀")
-        audio = open('./assets/welcome.mp3', 'rb')
-        bot.send_audio(message.chat.id, audio)
-        
+    
+    if checkClient(message.chat.id)==False:
+        client_list.append([message.chat.id,'0',0])
 
-        
-        bot.send_message(message.chat.id,"Let me know, how do you want to start make money? What do you want to do?",reply_markup=mainMarkup())
-        
+    bot.send_sticker(message.chat.id, "CAADAQADAQADHYJ7HYCiE4crvJ4kAg")
+    bot.send_message(message.chat.id, "✅✅ He he hey! My name is Bitcoin Oracle and I'm so excited to help you and to be your favourite trending tool")
+    bot.send_message(message.chat.id, "With this bot you can analyze all tweets in the last hours of Bitcoin. It enable to you make decisions that can take huge profits. If you want to be financially independently, Bitcoin Oracle is you best mate. 🚀🚀")
+    audio = open('./assets/welcome.mp3', 'rb')
+    bot.send_audio(message.chat.id, audio)
+    
+    bot.send_message(message.chat.id,"Let me know, how do you want to start make money? Which coin do you want to check?")
+
+
+@bot.message_handler(commands=['1h', '2h', '3h'])
+def command_hour(message):
+    
+    hour = int(message.text.replace("h", "").replace("/", ""))
+
+    setClient(message.chat.id,hour=hour)
+    bot.send_message(message.chat.id, "Processing")
+    bot.send_message(message.chat.id, "Aqui tornariem els resultats")
+    
+    resetClient(message.chat.id)
+    bot.send_message(message.chat.id, "Which coin do you want to check next?")
+    
+
+
 @bot.message_handler(func=lambda message: message.text == 'Show the analysis of the last hour' and message.content_type == 'text')
 def command_answer1(message):
     
+
     #CRIDA DE FUNCIONS NECESSARIES
     bot.send_message(message.chat.id, "Aqui mostrem estadistiques i emojis i blablabla")
     bot.send_message(message.chat.id, "So... It seems...")
@@ -51,10 +92,32 @@ def command_answer2(message):
     bot.send_message(message.chat.id, "6000. OJU! HARDCODEJAT!")
     
 
+
+
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def command_default(message):
-    bot.send_sticker(message.chat.id, "CAADAQADDgADHYJ7HdRDi-MonN_9Ag")
-    bot.send_message(message.chat.id, "I don't understand. What I'm gonna do?")
+    
+    coin=message.text.lower().capitalize()
+    
+    if coinExist('e'):
+
+        setClient(message.chat.id,coin=coin)
+
+
+        markup = types.ReplyKeyboardMarkup(row_width=1)
+        itembtn1 = types.KeyboardButton('/1h')
+        itembtn2 = types.KeyboardButton('/2h')
+        itembtn3 = types.KeyboardButton('/3h')
+        markup.add(itembtn1, itembtn2, itembtn3)
+
+        bot.send_message(message.chat.id, "How many hours?",reply_markup=markup)
+   
+    else:
+
+        bot.send_sticker(message.chat.id, "CAADAQADDgADHYJ7HdRDi-MonN_9Ag")
+        bot.send_message(message.chat.id, "I don't understand. What I'm gonna do?")
+
+
 
 bot.polling()
 
